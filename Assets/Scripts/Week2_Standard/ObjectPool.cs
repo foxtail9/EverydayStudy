774df6f8b1d1a0f1b66f6c7c066040634fbe0c75
GEUN SEOK <74439615+foxtail9@users.noreply.github.com>
2024-10-21 13:44:02 +0900

    Q2.[구현사항2]

--- Assets/Scripts/Week2_Standard/ObjectPool.cs
+++ Assets/Scripts/Week2_Standard/ObjectPool.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    //Q2.[구현사항 2]
+    private Dictionary<string, List<GameObject>> pools = new Dictionary<string, List<GameObject>>();
+
     public GameObject prefab;
     private List<GameObject> pool = new List<GameObject>();
     public int poolSize = 300;
@@ -19,6 +22,10 @@ public class ObjectPool : MonoBehaviour
             pool.Add(obj);
         }
 
+        //Q2.[구현사항 2] - Dictionary생성
+        CreatePool("Monster", prefab, poolSize);
+
+        DictionaryGet("Monster"); //Dictionary pools ""키값으로 비활성화된 gbj 활성화
     }
 
     public GameObject Get()
@@ -38,5 +45,40 @@ public class ObjectPool : MonoBehaviour
     {
         // 게임오브젝트를 deactive한다.
         obj.SetActive(false);
+    }
+
+    public void CreatePool(string key,GameObject prefab,int poolsiz)
+    {
+        pools[key] = new List<GameObject>();
+        for (int i = 0; i < poolsiz; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+
+    public GameObject DictionaryGet(string key)
+    {
+        if (pools.ContainsKey(key))
+        {
+            foreach (GameObject obj in pools[key])
+            {
+                if (!obj.activeInHierarchy)
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void DictionaryRelease(string key, GameObject obj)
+    {
+        if (pools.ContainsKey(key))
+        {
+            obj.SetActive(false);
+        }
     }
 }
