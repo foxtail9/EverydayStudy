0c9b2e3b370a2a0dda6afb59c5980960a0d65ed1
GEUN SEOK <74439615+foxtail9@users.noreply.github.com>
2024-10-25 18:53:42 +0900

    (Fix)Q2.List,Dictionary GameObject Pool다시 구현

--- Assets/Scripts/Week2_Standard/ObjectPool.cs
+++ Assets/Scripts/Week2_Standard/ObjectPool.cs
@@ -1,37 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    public GameObject Monster;
+    public int poolnum = 300;
+    List<GameObject> pool = new List<GameObject>();
     //Q2.[구현사항 2]
-    private Dictionary<string, List<GameObject>> pools = new Dictionary<string, List<GameObject>>();
+    Dictionary<string, List<GameObject>> pools = new Dictionary<string, List<GameObject>>();
 
-    public GameObject prefab;
-    private List<GameObject> pool = new List<GameObject>();
-    public int poolSize = 300;
-    public int countgameobject = 0;
 
-    void Start()
+    public void Start()
     {
-        //Q2.[구현사항 1]
-        // 미리 poolSize만큼 게임오브젝트를 생성한다.
-        for (int i = 0; i < poolSize; i++)
+        //Q2.[List]
+        MonsterPool(poolnum);
+        //Q2.[Dictionary]
+        MonsterPools("Monster", poolnum);
+    }
+
+    //Q2.[구현사항 1]
+    public void MonsterPool(int num)
+    {
+        for (int i = 0; i < num; i++)
         {
-            GameObject obj = Instantiate(prefab);
+            GameObject obj = Instantiate(Monster);
             obj.SetActive(false);
             pool.Add(obj);
         }
-
-        //Q2.[구현사항 2] - Dictionary생성
-        CreatePool("Monster", prefab, poolSize);
-
-        DictionaryGet("Monster"); //Dictionary pools ""키값으로 비활성화된 gbj 활성화
     }
-    //Q2.[구현사항 1] : get
-    public GameObject Get()
+
+    //Q2.[구현사항 1] - Get
+    public GameObject PoolGet()
+    {
+        foreach(GameObject obj in pool)
+        {
+            obj.SetActive(true);
+            return obj;
+        }
+        return null;
+    }
+    //Q2.[구현사항 1] - Release
+    public void PoolRelease()
     {
         foreach (GameObject obj in pool)
+        {
+            if (obj.activeInHierarchy)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    //Q2.[구현사항 2]
+    public void MonsterPools(string poolKey, int num)
+    {
+        pools[poolKey] = new List<GameObject>();
+
+        for (int i = 0; i < num; i++)
+        {
+            GameObject obj = Instantiate(Monster);
+            obj.SetActive(false);
+            pools[poolKey].Add(obj);
+        }
+    }
+    //Q2.[구현사항 2] - Get
+    public GameObject PoolsGet(string poolKey)
+    {
+        foreach (GameObject obj in pools[poolKey])
         {
             if (!obj.activeInHierarchy)
             {
@@ -41,46 +78,16 @@ public class ObjectPool : MonoBehaviour
         }
         return null;
     }
-    //Q2.[구현사항 1] : Release
-    public void Release(GameObject obj)
-    {
-        // 게임오브젝트를 deactive한다.
-        obj.SetActive(false);
-    }
 
-    //Q2.[구현사항 2] : Dictionary키 할당하기
-    public void CreatePool(string key,GameObject prefab,int poolsiz)
+    //Q2.[구현사항 2] - Release
+    public void PoolsRelease(string poolKey)
     {
-        pools[key] = new List<GameObject>();
-        for (int i = 0; i < poolsiz; i++)
+        foreach (GameObject obj in pools[poolKey])
         {
-            GameObject obj = Instantiate(prefab);
-            obj.SetActive(false);
-            pool.Add(obj);
-        }
-    }
-    //Q2.[구현사항 2] : Dictionary pools key값으로 찾아가 활성화
-    public GameObject DictionaryGet(string key)
-    {
-        if (pools.ContainsKey(key))
-        {
-            foreach (GameObject obj in pools[key])
+            if (obj.activeInHierarchy)
             {
-                if (!obj.activeInHierarchy)
-                {
-                    obj.SetActive(true);
-                    return obj;
-                }
+                obj.SetActive(false);
             }
-        }
-        return null;
-    }
-    //Q2.[구현사항 2] : Dictionary pools key값으로 찾아가 비활성화
-    public void DictionaryRelease(string key, GameObject obj)
-    {
-        if (pools.ContainsKey(key))
-        {
-            obj.SetActive(false);
         }
     }
 }
